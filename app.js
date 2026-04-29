7e7732e8988e728b1b411e29263ab74e1353c5ff
113121104-commits <113121104@gms.tcu.edu.tw>
2026-04-29 15:39:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require('express');
-const app = express();
+const line = require('@line/bot-sdk');
 
+const app = express();
 app.use(express.json());
+
+// 🔑 LINE 設定
+const config = {
+  channelAccessToken: '2009928969',
+  channelSecret: '3802ab5e60a04f27174f68eb94f08a89'
+};
+
+const client = new line.Client(config);
 
 // webhook
 app.post('/webhook', (req, res) => {
-    console.log('收到訊息:', req.body);
+  Promise.all(req.body.events.map(event => {
+    if (event.type === 'message' && event.message.type === 'text') {
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: '你好，我是機器人 🤖'
+      });
+    }
+  }));
 
-    // 固定回覆（先只做 log）
-    res.send('OK');
+  res.sendStatus(200);
 });
 
 app.get('/', (req, res) => {
-    res.send('Bot is running');
+  res.send('Bot is running');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('機器人啟動');
+  console.log('機器人啟動');
 });
